58589544be78e944f83f7eaac4cd34096dc88df8
ioutput <935436445@qq.com>
2019-04-21 16:38:52 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import { Provider} from 'mobx-react'
+import { LocaleProvider } from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import store from './store'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+    <LocaleProvider locale={zh_CN}>
+      <Provider {...store}>
+        <App/>
+      </Provider>
+    </LocaleProvider>
+  </BrowserRouter>
+	, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/views/index/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './index.css';
+import {
+  Button
+} from 'antd';
+class App extends Component {
+  render() {
+    return (
+        <Button type="primary">primary</Button>
+    );
+  }
+}
+
+export default App;
